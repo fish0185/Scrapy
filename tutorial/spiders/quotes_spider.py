e24c691eb88e870de8faf64c406024291c55f2de
DESKTOP-JKIBI8I\Gary <igary.yu@gmail.com>
2017-11-01 00:07:49 +1100

    chore

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -12,24 +12,41 @@ class QuotesSpider(scrapy.Spider):
         if jsonresponse['data'] is not None:
             for row in jsonresponse['data']['rows']:
                 detailPageUrl = 'http://www.sydneytoday.com/flea_market/' + row['_id']
-                yield response.follow(detailPageUrl, callback=self.parseDetailPage)
+                hardCode = 'http://www.sydneytoday.com/flea_market/181744240375004'
+                yield response.follow(detailPageUrl, callback=self.parseDetailPage, meta=row)
 
             pageCount = int(response.url[52:]) + 1
-            if pageCount < 2:
+            self.logger.info('Parse page ====================> %s', pageCount)
+            if pageCount < 10:
                 nextPageUrl = 'http://www.sydneytoday.com/flea_market-cg0-dl0-bs0-p' + str(pageCount)
                 yield response.follow(nextPageUrl, callback=self.parse)
 
     def parseDetailPage(self, response):
-        content = response.css('div.yp-content .yp-detail ::text')[0].extract()
-        content1 = response.css('div.yp-content .yp-detail ::text')[1].extract()
-        content2 = response.css('div.yp-content .yp-detail ::text')[2].extract()
-        content3 = response.css('div.yp-content .yp-detail ::text')[3].extract()
-        content4 = response.css('div.yp-content .yp-detail ::text')[4].extract()
-        content5 = response.css('div.yp-content .yp-detail ::text')[5].extract()
+        productContent = response.css('div.yp-content .col-xs-6')[0]
+        #category = productContent.xpath('.//div/span/text()')[1].extract()
+        pri = productContent.css('.yp-detail::text')
+        contactContent = response.css('div.yp-content .col-xs-6')[1]
+        #chat = contactContent.xpath('.//img[contains(@src, "yp-contact-wechat.png")]/../text()').extract_first()
+        #email = contactContent.xpath('.//img[contains(@src, "yp-contact-email.png")]/following-sibling::img[1]/@src').extract_first()
+        #if contactContent.xpath('.//img[contains(@src, "yp-contact-wechat.png")]/@src').extract_first() is not None:
+
         yield {
             'title': response.css('div.yp-content h1::text').extract_first(),
-            'price': response.css('div.yp-content .yp-detail')[3].extract(),
-            'description': response.css('div.yp-descriprion::text').extract_first()
+            'price': response.meta['jiage'],
+            'mobile': response.meta['mobile'],
+            'contact': response.meta['contact'],
+            'uid': response.meta['uid'],
+            'pageUrl': response.url,
+            'pageId': response.meta['_id'],
+            'changed': response.meta['changed'],
+            'buyOrSell': response.meta['buysells'],
+            'deliverys': response.meta['deliverys'],
+            'pubTime': response.meta['pub_time'],
+            'coverImage': response.meta['cover'],
+            'categoryText': productContent.xpath('.//div/span/text()')[1].extract(),
+            'description': response.css('div.yp-descriprion::text').extract_first(),
+            'emailUrl': contactContent.xpath('.//img[contains(@src, "yp-contact-email.png")]/following-sibling::img[1]/@src').extract_first(),
+            'wechat': contactContent.xpath('.//img[contains(@src, "yp-contact-wechat.png")]/../text()').extract_first()
         }
 
 
